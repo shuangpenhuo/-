25ef0cb33d42e5a5101db5f552b97c025c7771c7
shuangpenhuo <123225973+shuangpenhuo@users.noreply.github.com>
2023-04-02 18:41:04 -0400

    Update 123.cpp

--- 123.cpp
+++ 123.cpp
@@ -5,5 +5,4 @@
 
 // Notice the backslash ('\') at the end of each internal line of
 // the macro to continue it to the next line.
-
 // Look in the Tests/ directory to see how this macro will be used.
